db799c3c80f0ae7d3cef2cb97499fa5a6f8cd231
Sang <sang.d.le@lawrence.edu>
2018-11-16 23:47:30 -0600

    Updates

---
+++ precache-manifest.6173d2185381360099bb357bf9575149.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Turmite-Games/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dc932c5114fb8aaeb5d6",
+    "url": "/Turmite-Games/static/js/runtime~main.dc932c51.js"
+  },
+  {
+    "revision": "9878ddad016059c6abf2",
+    "url": "/Turmite-Games/static/js/main.9878ddad.chunk.js"
+  },
+  {
+    "revision": "99980f7ad521b01630a3",
+    "url": "/Turmite-Games/static/js/1.99980f7a.chunk.js"
+  },
+  {
+    "revision": "9878ddad016059c6abf2",
+    "url": "/Turmite-Games/static/css/main.ee49e56a.chunk.css"
+  },
+  {
+    "revision": "9798ceb2f483d1a61252b07cdc110956",
+    "url": "/Turmite-Games/index.html"
+  }
+];
